d5c4134d99ea93a5ab68dc4b26b555cddc8f0933
PAZ-AVM <PAZ-AVM@users.noreply.github.com>
2023-03-20 15:56:33 +0100

    New Priting ;D !

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,9 @@
-print("Hello Li Qun")
+print("Hello Li Qun :D")
 
 
 def start():
-    print("Hello Andres and Nora! love to PAZ from tokyo!")
+    print("Hello Andres and Nora! love to PAZ from tokyo :DDDD !")
+    print("This is Cooolio ;D !!!")
 
 
 start()
